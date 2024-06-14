0e22d7f1e9835ecba1b7c2994c264b51c742ae45
pastapentagon <quincyhjamieson@gmail.com>
2024-06-14 10:33:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var number = 10;
 var goodmsg 'the number is 10';
 var badmsg = 'the number isnt 10';
-if(numeber == 10){
-  document.getElementById('box').innerHTML = goodmsg;
-}else{
-  document.getElementById('box').innerHTML = 'badmsg';
-}
 
+document.getElementById('box').innerHTML = number;
